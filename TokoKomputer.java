f85c1ffef263460cdae03c6870ea373ddf7e1ef9
eridz <eridz.ufazy@gmail.com>
2024-02-28 21:26:56 +0700

    menambah option merubah jumlah PCS

--- TokoKomputer.java
+++ TokoKomputer.java
@@ -1,3 +1,5 @@
+//package TokoKomputer;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
@@ -6,93 +8,167 @@ import java.util.HashMap;
 public class TokoKomputer {
     public static String addTab(String text, int start) {
         String result = "";
-        if (text.length() < (8 - 2)) {
+        if (text.length() < (8 - start)) {
+            result = text + "\t\t\t\t\t\t";
+        } else if (text.length() < (16 - start)) {
             result = text + "\t\t\t\t\t";
-    	} else if (text.length() < (16 - start)) {
+        } else if (text.length() < (24 - start)) {
             result = text + "\t\t\t\t";
-    	} else if (text.length() < (24 - start)) {
+        } else if (text.length() < (32 - start)) {
             result = text + "\t\t\t";
-    	} else if (text.length() < (32 - start)) {
+        } else if (text.length() < (40 - start)) {
             result = text + "\t\t";
-    	} else if (text.length() < (40 - start)) {
+        } else if (text.length() < (48 - start)) {
             result = text + "\t";
         }
-    	
-    	return result;
+        return result;
     }
     
 	public static void main (String[] args) {
+        Boolean selectItem = true;
+        Boolean showListProducts = true;
 		Scanner scanner = new Scanner(System.in);
 		HashMap<Integer, Integer> shooper = new HashMap<>();
 		NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("id", "ID"));
-		String dotedHr = "|---------------------------------------------------------|";
-		String solidHr = "__________________________________________________________";
+		String dotedHr = "|--------------------------------------------------------------------|";
+		String solidHr = "_____________________________________________________________________";
 		
 		Product[] Products = {
-    	new Product("Tinta Printer", 95000), 
-    	new Product("Mouse Wireless", 65000), 
-    	new Product("Flashdisk 64GB", 87500), 
-    	new Product("Kabel Konektor", 15000), 
-    	new Product("Paket Anti Virus", 200000), 
-    	new Product("Keyboard Wireless", 850000),
-    	new Product("Mouse Pad", 80000),
-    	new Product("Earphone Bluetooth TWS A30S", 98000),
-    	new Product("VGA NVIDIA RTX 3080", 37820000)
-    	
-    	};
-    	
-		while (true) {
-			System.out.printf("%s_\n|\n| List Produk Yang Tersedia\n%s\n",solidHr, dotedHr);
+            new Product("Tinta Printer", 95000), 
+            new Product("Mouse Wireless Goojodoq", 65000), 
+            new Product("Flashdisk 64GB", 87500), 
+            new Product("Kabel Konektor", 15000), 
+            new Product("Paket Anti Virus", 200000), 
+            new Product("Keyboard Wireless", 850000),
+            new Product("Mouse Pad TAFFGO MP002", 80000),
+            new Product("Earphone Bluetooth TWS A30S", 98000),
+            new Product("VGA NVIDIA RTX 3080", 37820000),
+            new Product("Arduino Uno", 53500),
+            new Product("ESP 8266", 37800)
+        };
+     	
+		while (selectItem == true) {
+            String itemNameSelected = "";
+			int indexItemSelected = 0; 
+			
+			if (showListProducts == true) {
+                System.out.printf("%s_\n|\n| List Produk Yang Tersedia\n%s\n",solidHr, dotedHr);
+                // menampilkan list produk dengan looping
+                for (int i = 0; i < Products.length; i++) {
+                    System.out.printf("| %s- %s \n", addTab(Products[i].Name(), 2), currencyFormat.format(Products[i].Prize()));
+                }
+                System.out.println("\nTulis Nama Barang Dengan \"Lengkap\" Untuk Memilih!");
+            }
+            // menyimpan pilihan pengguna
+            itemNameSelected = scanner.nextLine().toLowerCase();
+			
 			for (int i = 0; i < Products.length; i++) {
-		       	 System.out.printf("| %s- %s \n",addTab(Products[i].Name(), 2), currencyFormat.format(Products[i].Prize()));
-			}
-			
-			System.out.println("\nTulis Nama Barang Dengan \"Lengkap\" Untuk Memilih!");
-			String itemName = scanner.nextLine().toUpperCase();
-			
-			int item = 0; 
-			
-			for (int i = 0; i < Products.length; i++) {
-				if (itemName.equals(Products[i].Name().toUpperCase())) {
-					item = i + 1;
+                // mengecek apakah pilihan valid
+				if (itemNameSelected.equals(Products[i].Name().toLowerCase())) {
+				    // jika valid simpan index produk
+					indexItemSelected = i + 1;
+				} else {
+				    // list produk tidak akan ditampilkan
+					showListProducts = false;
 				}
 			}
 			
-			if (item <= Products.length && item > 0) {
-			      	
-	        	int pcs = 1;
-		        for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
-			        if (entry.getKey() == item) {
-			        	pcs += entry.getValue();
-			        }
-		        }
-		        
-		        shooper.put(item, pcs);
-		        System.out.println("\n_" + solidHr + "\n| PCS  NAMA BARANG \t\t\t| HARGA\n" + dotedHr);
-		        for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
-    	       		System.out.printf("|  %s   %s: %s\n", entry.getValue().toString(), addTab(Products[entry.getKey() - 1].Name(), 7), currencyFormat.format(Products[entry.getKey() - 1].Prize()), entry.getValue().toString());
-		        }
-		        System.out.printf("\nAnda Telah Memasukkan => %s\nKetik $ (bayar) atau + (tambah) atau - (mengurangi)\n", Products[item - 1].Name());
-		        String end = scanner.nextLine().toLowerCase();
-		        if (end.equals("$")) {
-					int totalBiaya = 0;
-		        	System.out.printf("_%s\n|\n|\t\t    TOKO JAYA KOMPUTER \n|\t\t JL.Juanda No.64 Surabaya \n|%s\n", solidHr, solidHr);
-				    for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
-				    	int biaya = Products[entry.getKey() - 1].Prize() * entry.getValue();
-			    	  	System.out.printf("| %s %s: %s\n", entry.getValue().toString(), addTab(Products[entry.getKey() - 1].Name(), 4), currencyFormat.format(biaya));
-					    totalBiaya += biaya;
-					}
-					System.out.println(dotedHr);
-					System.out.printf("| * TOTAL BIAYA\t\t\t\t: %s\n|%s", currencyFormat.format(totalBiaya), solidHr);
-	    			return;
-		        }
-	        } else {
-	        	System.out.println(solidHr + "_\nInputan Tidak Valid. Silahkan Masukkan Ulang!");
+			// jika index != 0 maka akan dijalankan
+			if (indexItemSelected != 0) {
+                // inisialisasi nilai default pcs 
+                int pcs = 1;
+                // inisialisasi nilai default inputOptionValid 
+                Boolean inputOptionValid = true;
+                // melooping keranjang belanjaan
+                for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
+                    // jika barang terpilih sudah ada di keranjang
+                    if (entry.getKey() == indexItemSelected) {
+                        // tambahkan 1 pcs lagi
+                        pcs += entry.getValue();
+                    }
+                }
+                
+                // memperbarui barang dan pcs di keranjang belanjaan
+                shooper.put(indexItemSelected, pcs);
+                System.out.println("\n_" + solidHr + "\n| PCS  NAMA BARANG \t\t\t\t| HARGA\n" + dotedHr);
+                // menampilkan isi keranjang belanjaan dengan looping
+                for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
+                    System.out.printf("|  %s   %s: %s\n", entry.getValue().toString(), addTab(Products[entry.getKey() - 1].Name(), 7), currencyFormat.format(Products[entry.getKey() - 1].Prize()), entry.getValue().toString());
+                }
+                // menampilkan dialog opsi ( $ / + / PCS )
+                System.out.printf("\nAnda Telah Menambahkan %s\nKetik $ (bayar) atau + (tambah) atau PCS (Change PCS) : ", Products[indexItemSelected - 1].Name());
+                
+                // mengecek nilai inputOptionValid (default = true)
+                while (inputOptionValid) {
+                    // meminta pengguna memilih opsi ( $ / + / PCS )
+                    String inputOption = scanner.nextLine().toLowerCase();
+                    // mengecek Pilih pengguna
+                    if (inputOption.equals("$")) {
+                        // memberhentikan pengguna untuk memilih barang
+                        selectItem = false;
+                        // memberhentikan meminta pengguna memilih opsi ( $ / + / PCS )
+                        inputOptionValid = false;
+                    } else if (inputOption.equals("+")) {
+                        // mengulangi pengguna untuk memilih barang
+                        selectItem = true;
+                        // memberhentikan meminta pengguna memilih opsi ( $ / + / PCS )
+                        inputOptionValid = false;
+                        // menampilkan list produk
+                        showListProducts = true;
+                    } else if (inputOption.equals("pcs")) {
+                        System.out.println("\n_" + solidHr + "\n| PCS  NAMA BARANG \t\t\t\t| HARGA\n" + dotedHr);
+                        for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
+                            System.out.printf("|  %s   %s: %s\n", entry.getValue().toString(), addTab(Products[entry.getKey() - 1].Name(), 7), currencyFormat.format(Products[entry.getKey() - 1].Prize()), entry.getValue().toString());
+                        }
+                        System.out.println("\nSilahkan Masukkan Nama Barang Yang Ingin Diubah Jumlah PCS-nya!");
+                        String ChangePCS = scanner.nextLine();
+                        for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
+                            if (ChangePCS.equals(Products[entry.getKey() - 1].Name())) {
+                                System.out.printf("Masukkan Berapa PCS Untuk Produk %s : ", Products[entry.getKey() - 1].Name());
+                                int countPCS = scanner.nextInt();
+                                shooper.put(entry.getKey(), countPCS);
+                                System.out.println("\n_" + solidHr + "\n| PCS  NAMA BARANG \t\t\t\t| HARGA\n" + dotedHr);
+                                for (HashMap.Entry<Integer, Integer> item : shooper.entrySet()) {
+                                    System.out.printf("|  %s   %s: %s\n", item.getValue().toString(), addTab(Products[item.getKey() - 1].Name(), 7), currencyFormat.format(Products[item.getKey() - 1].Prize()), item.getValue().toString());
+                                }
+                                System.out.printf("\nAnda Telah Mengubah PCS Produk %s\n", Products[entry.getKey() - 1].Name());
+                            }
+                        }
+                    } else {
+                        // menampilkan dialog jika input tidak Valid
+                        System.out.print("Silahkan Pilih ( $ / + / PCS ) : ");
+                    }
+                }
+            // menampilkan dialog jika input tidak Valid
+            } else {
+                // inisialisasi underlineDash dengan String kosong
+                String underlineDash = "";
+                // membuat underline pada input tidak Valid
+                for (int i = 0; i < itemNameSelected.length(); i++) {
+                    // menambahkan - sebanyak karakter inputnya
+                    underlineDash += "-";
+                }
+                System.out.printf("%s\nTidak Valid! Silahkan Masukkan Nama Yang Sesuai!!\n", underlineDash);
 	        }
 		}
+        // menampilkan header 'nota'
+        System.out.printf("_%s\n|\n|\t\t\t    TOKO JAYA KOMPUTER \n|\t\t\t JL.Juanda No.64 Surabaya \n|%s\n", solidHr, solidHr);
+        // inisialisasi totalBiaya dengan nilai 0
+		int totalBiaya = 0;
+		// melooping isi keranjang untuk 'nota'
+        for (HashMap.Entry<Integer, Integer> entry : shooper.entrySet()) {
+            // mendeklarasikan nilai biaya pada setiap barang
+		    int biaya = Products[entry.getKey() - 1].Prize() * entry.getValue();
+		    // menjumlahkan nilai biaya
+			totalBiaya += biaya;
+			// menampilkan isi keranjang
+            System.out.printf("| %s %s: %s\n", entry.getValue().toString(), addTab(Products[entry.getKey() - 1].Name(), 4), currencyFormat.format(biaya));
+		}
+        // menampilkan total biaya
+		System.out.printf("%s\n| * TOTAL BIAYA\t\t\t\t\t: %s\n|%s", dotedHr, currencyFormat.format(totalBiaya), solidHr);
 	}
 }
-
+// membuat class baru yang berfungsi seperti objek di javascript 
 class Product {
 
 	private String _name;
